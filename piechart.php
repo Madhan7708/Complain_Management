f2f71927e22c86236cee7fd42d98be42de467a1c
Madhan7708 <bashamadhan002@gmail.com>
2024-09-05 22:10:12 +0530

    added

--- piechart.php
+++ piechart.php
@@ -1,20 +1,6 @@
 <?php
 header('Content-Type: application/json');
-
-// Database connection settings
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "complaints";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+include('db.php');
 // Query to get counts of each type_of_problem
 $sql = "SELECT type_of_problem, COUNT(*) as count FROM complaints_detail GROUP BY type_of_problem";
 $result = $conn->query($sql);
