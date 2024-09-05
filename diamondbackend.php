0c10f218decacacc7aa87b000c4f0023af19f549
Madhan7708 <bashamadhan002@gmail.com>
2024-09-05 21:05:17 +0530

    added

---
+++ diamondbackend.php
@@ -0,0 +1,49 @@
+<?php
+// get_complaints.php
+include 'db.php'; // Include your database connection file
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Fetch total complaints
+    $query1 = "SELECT COUNT(*) as complain FROM complaints_detail WHERE status > 0";
+    $output1 = mysqli_query($conn, $query1);
+    $row1 = mysqli_fetch_assoc($output1);
+    $complainCount = $row1['complain'];
+
+    // Fetch completed complaints
+    $query2 = "SELECT COUNT(*) as completed FROM complaints_detail WHERE status = '13'";
+    $output2 = mysqli_query($conn, $query2);
+    $row2 = mysqli_fetch_assoc($output2);
+    $completedCount = $row2['completed'];
+
+    //Fetch inprogress completed
+    $query3 = "SELECT COUNT(*) as inprogress FROM complaints_detail WHERE status = '10'";
+    $output3 = mysqli_query($conn, $query3);
+    $row3 = mysqli_fetch_assoc($output3);
+    $inprogressCount = $row3['inprogress'];
+
+    //fetch pending status
+    $query4 = "SELECT COUNT(*) as pending FROM complaints_detail WHERE (status ='1' or status ='2' or status='4' or  status='7' or status='8')";
+    $output4 = mysqli_query($conn, $query4);
+    $row4 = mysqli_fetch_assoc($output4);
+    $pendingCount = $row4['pending'];
+
+    //fetch reassigned work
+
+    $query5 = "SELECT COUNT(*) as reassign FROM complaints_detail WHERE (status ='14')";
+    $output5 = mysqli_query($conn, $query5);
+    $row5 = mysqli_fetch_assoc($output5);
+    $reassignCount = $row5['reassign'];
+
+    // Return the counts as a JSON object
+    echo json_encode(array(
+        "complainCount" => $complainCount,
+        "completedCount" => $completedCount,
+        "inprogressCount" => $inprogressCount,
+        "pendingCount" => $pendingCount,
+        "reassignCount" => $reassignCount
+
+
+    ));
+} else {
+    echo json_encode(array("error" => "Invalid request method."));
+}
