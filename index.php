24fcac68ca5599ca200e6bb84788e3be811f11cf
Madhan7708 <bashamadhan002@gmail.com>
2024-09-07 17:34:34 +0530

    added

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-45">
                         <li class="sidebar-item" > <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.php" aria-expanded="false"><i class="mdi mdi-view-dashboard" ></i><span class="hide-menu">Dashboard</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="requirement.html" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Requirements</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="requirements.php" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Requirements</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="complain.php" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span class="hide-menu">Complain Status</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="tables.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Change Password</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="status.php" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Work Category</span></a></li>
@@ -258,7 +258,7 @@
                                 <div class="diamond" style="background-color: #47b2c8  ;">
                                   <div>
                                     <h5 class="text-white">Request</h5>
-                                    <h5 class="text-white">3</h5>
+                                    <h5 class="text-white"><span id="requestCount"></span></h5>
                                   </div>
                                 </div>
                               </div>
@@ -346,7 +346,8 @@ function fetchComplaintCounts() {
               $('#complainCount').text(response.complainCount);
               $('#completedCount').text(response.completedCount);
               $('#inprogresscount').text(response.inprogressCount);
-              $('#pendingCount').text(response.pendingCount);       
+              $('#pendingCount').text(response.pendingCount);  
+              $('#requestCount').text(response.requestCount);  
               $('#reassignCount').text(response.reassignCount);
 
 
@@ -355,6 +356,7 @@ function fetchComplaintCounts() {
               $('#completedCount').text("Error fetching data");
               $('#inprogresscount').text("Error fetching data");
               $('#pendingCount').text("Error fetching data");
+              $('#requestCount').text("Error fetching data"); 
               $('#reassignCount').text("Error fetching data");
 
 
@@ -365,6 +367,7 @@ function fetchComplaintCounts() {
           $('#completedCount').text("Error with AJAX request");
           $('#inprogresscount').text("Error with AJAX request");
           $('#pendingcount').text("Error with AJAX request");
+          $('#requestCount').text("Error with AJAX request"); 
           $('#reassigncount').text("Error with AJAX request");
 
       }
