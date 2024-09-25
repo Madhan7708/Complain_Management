353ddd6cfcbf344fdeb9ecddb1760d481d300942
Madhan7708 <bashamadhan002@gmail.com>
2024-09-25 19:30:52 +0530

    added sql files

--- complaints.sql
+++ complaints.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2024 at 06:15 AM
+-- Generation Time: Sep 24, 2024 at 12:37 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,22 @@ CREATE TABLE `comments` (
   `reason` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `problem_id`, `reason`) VALUES
+(34, 67, ''),
+(35, 68, ''),
+(36, 69, ''),
+(37, 70, ''),
+(38, 63, ''),
+(39, 64, ''),
+(40, 66, ''),
+(41, 67, ''),
+(42, 68, ''),
+(43, 65, '');
+
 -- --------------------------------------------------------
 
 --
@@ -52,53 +68,53 @@ CREATE TABLE `complaints_detail` (
   `problem_description` varchar(400) NOT NULL,
   `images` varchar(255) NOT NULL,
   `date_of_reg` date NOT NULL,
-  `days_to_complete` date NOT NULL,
+  `days_to_complete` varchar(100) NOT NULL,
   `status` varchar(200) NOT NULL,
   `feedback` varchar(250) NOT NULL,
+  `reason` varchar(255) NOT NULL,
   `task_completion` varchar(250) NOT NULL,
-  `date_of_completion` date NOT NULL
+  `date_of_completion` date NOT NULL,
+  `reassign_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `complaints_detail`
 --
 
-INSERT INTO `complaints_detail` (`id`, `faculty_id`, `faculty_name`, `department`, `faculty_contact`, `faculty_mail`, `block_venue`, `venue_name`, `type_of_problem`, `problem_description`, `images`, `date_of_reg`, `days_to_complete`, `status`, `feedback`, `task_completion`, `date_of_completion`) VALUES
-(43, 11111111, 'nalin kumar', 'EEE', '8883711939', 'nalinkumar@gmail.com', 'APJ-106', 'c', 'Electrical Work', 'fan not working', '1.jpeg', '2024-08-27', '0000-00-00', '9', 'sdfds', '0', '0000-00-00'),
-(44, 11111111, 'nalin kumar', 'EEE', '8883711939', 'nalinkumar@gmail.com', 'APJ-106', 'c2', 'Electrical Work', 'fan not working', '2.jpeg', '2024-08-27', '0000-00-00', '19', 'hello', '0', '0000-00-00'),
-(45, 11111111, 'hariharan', 'cse', '56789', 'nalinkumar@gmail.com', 'APJ-106', 'c4', 'Electrical Work', 'fan not working', '3.jpg', '2024-08-27', '2024-09-06', '9', '', '0', '0000-00-00'),
-(46, 11111111, 'hariharan', 'cse', '56789', 'nalinkumar@gmail.com', 'APJ-106', 'departmenc3', 'Electrical Work', 'fan not working', '4.jpg', '2024-08-27', '0000-00-00', '7', '', '0', '0000-00-00'),
-(47, 11111111, 'hariharan', 'cse', '56789', 'nalinkumar@gmail.com', 'APJ-106', 'c3', 'Electrical Work', 'fan not working', '5.jpeg', '2024-08-27', '0000-00-00', '7', '', '0', '0000-00-00'),
-(48, 0, 'Rabin', 'cse', '9003581769', 's', 's', 'c5', 'Carpenter Work', 'er', '1.jpeg', '2024-08-27', '2024-09-12', '7', '', '0', '0000-00-00'),
-(49, 0, 'nalin', 'EEE', '9003581769', 'd', 'RK-206', 'c6', 'Electrical Work', 'er', '2.jpeg', '2024-08-27', '0000-00-00', '13', '', '0', '0000-00-00'),
-(50, 0, 'hariharan', 'EEE', '8883711939', 'nalinkumar@gmail.com', 'APJ-106', 'c7', 'Electrical Work', 'fan not working', '1.jpeg', '2024-08-27', '2024-09-12', '8', '', '0', '0000-00-00'),
-(51, 0, 'Rabin', 'cse', '9003581769', 's', 's', 'c8', 'Electrical Work', 's', '4.jpg', '2024-08-27', '2024-09-11', '10', 'Now fan is working properly', 'fully completed', '2024-03-18'),
-(52, 0, 'hariharan', 'EEE', '9003581769', 'nalinkumar@gmail.com', 's', 'c9', 'Carpenter Work', 's', '1.jpeg', '2024-08-27', '0000-00-00', '10', 'wifi working properly', 'partilly completed', '2023-12-27'),
-(53, 0, 'saran', 'cse', '56789', 'kknN@', 'rg-45', 'c0', 'Electrical Work', 's', '1.jpeg', '2024-08-27', '2024-09-12', '7', '', '0', '0000-00-00'),
-(54, 0, 'hariharan', 'cse', '9003581769', 'rabinsmithsakthivel@gmail.com', 'RK-206', 'de', 'Carpenter Work', 's', '3.jpg', '2024-08-27', '2024-09-14', '9', 'not good', '0', '0000-00-00'),
-(55, 0, 'Rabin', 'cse', '9003581769', 'kknN@', 'RK-206', 'class', 'Civil Work', 'er', '2.jpeg', '2024-08-27', '0000-00-00', '16', 'not satisfied', '0', '0000-00-00'),
-(56, 234, 'Rabin', 's', '9003581769', 'rabinsmithsakthivel@gmail.com', 's', 'd', 'Electrical Work', 'er', '1.jpeg', '2024-08-27', '0000-00-00', '14', '', '0', '0000-00-00'),
-(58, 7410, 'nn', 'csbs', '78963', 'nbvcarty', 'vv', 'mn', 'electrical', 'nm', '1.jpeg', '2024-08-28', '2024-09-12', '7', 'nn', 'nn', '0000-00-00');
+INSERT INTO `complaints_detail` (`id`, `faculty_id`, `faculty_name`, `department`, `faculty_contact`, `faculty_mail`, `block_venue`, `venue_name`, `type_of_problem`, `problem_description`, `images`, `date_of_reg`, `days_to_complete`, `status`, `feedback`, `reason`, `task_completion`, `date_of_completion`, `reassign_date`) VALUES
+(63, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'RK-123', 'lab', 'Carpenter Work', 'broken table', '0000000011.png', '2024-09-24', '2024-09-26', '7', 'good work', '', 'Fully Completed', '2024-09-24', '2024-09-24'),
+(64, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'RK-206', 'class', 'Electrical Work', 'broken light', '0000000012.png', '2024-09-24', '2024-09-25', '16', 'bad idea', '', '', '0000-00-00', '2024-09-24'),
+(65, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'RK-206', 'class', 'Carpenter Work', 'broken table', '0000000013.jpg', '2024-09-24', '2024-09-25', '7', 'noo', '', 'Partially Completed', '2024-09-24', '2024-09-24'),
+(66, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'RK-206', 'class', 'Civil Work', 'crack on walls', '0000000014.jpg', '2024-09-24', '2024-10-01', '20', 'koo', '', 'Fully Completed', '2024-09-24', '2024-09-24'),
+(67, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'APJ-106', 'class', 'Partition Work', 'partition', '0000000015.jpg', '2024-09-24', '2024-10-05', '20', 'goo', '', 'Fully Completed', '2024-09-24', NULL),
+(68, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'RK-206', 'class', 'IT Infra Work', 'system problem', '0000000016.png', '2024-09-24', '2024-09-25', '15', 'not nice', '', '', '0000-00-00', '2024-09-24'),
+(69, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'APJ-106', 'class', 'Plumbing Work', 'water leakage', '0000000017.png', '2024-09-24', '2024-09-27', '20', 'jii', '', '', '0000-00-00', NULL),
+(70, 11111111, 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in', 'APJ-106', 'department', 'Civil Work', 'crack', '0000000018.png', '2024-09-24', '2024-10-01', '20', 'noo', '', '', '0000-00-00', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `faculty_login`
+-- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty_login` (
+CREATE TABLE `faculty` (
+  `id` int(20) NOT NULL,
   `faculty_id` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `password` varchar(15) NOT NULL,
+  `faculty_name` varchar(100) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  `faculty_contact` varchar(20) NOT NULL,
+  `faculty_mail` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `faculty_login`
+-- Dumping data for table `faculty`
 --
 
-INSERT INTO `faculty_login` (`faculty_id`, `password`) VALUES
-('11111111', '1234'),
-('22222222', '1234'),
-('33333333', '1234');
+INSERT INTO `faculty` (`id`, `faculty_id`, `password`, `faculty_name`, `department`, `faculty_contact`, `faculty_mail`) VALUES
+(1, '11111111', '1234', 'Ragul', 'IT', '9629613708', 'ragul@mkce.ac.in'),
+(2, '22222222', '1234', 'Nalin', 'EEE', '9663852741', 'nalin@mkce.ac.in'),
+(3, '33333333', '1234', 'Srihari', 'CSE', '963741852', 'sri@mkce.ac.in');
 
 -- --------------------------------------------------------
 
@@ -113,31 +129,21 @@ CREATE TABLE `manager` (
   `priority` varchar(25) NOT NULL,
   `comment_query` varchar(250) NOT NULL,
   `comment_reply` varchar(250) NOT NULL,
-  `query_date` date NOT NULL
+  `query_date` date NOT NULL,
+  `reply_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `manager`
 --
 
-INSERT INTO `manager` (`task_id`, `problem_id`, `worker_id`, `priority`, `comment_query`, `comment_reply`, `query_date`) VALUES
-(1, 48, 'ELECTRICAL', 'HIGH', 'hello', '', '0000-00-00'),
-(2, 49, 'ELECTRICAL', 'LOW', '', '', '0000-00-00'),
-(3, 50, 'ELECTRICAL', 'MEDIUM', 'hoo', '', '0000-00-00'),
-(4, 51, 'ELECTRICAL', 'LOW', 'hj', '', '2024-09-13'),
-(5, 52, 'CIVIL', 'LOW', 'hii', '', '2024-09-13'),
-(6, 53, 'CIVIL', 'LOW', 'hoo', '', '2024-09-13'),
-(7, 54, 'CIVIL', 'HIGH', '', '', '0000-00-00'),
-(8, 55, 'CIVIL', 'MEDIUM', '', '', '0000-00-00'),
-(9, 58, 'CARPENTRY', 'High', 'h', '', '0000-00-00'),
-(10, 56, 'PARTITION', 'High', 'hii', '', '0000-00-00'),
-(11, 44, 'ELECTRICAL', 'Low', '', '', '0000-00-00'),
-(12, 45, 'PLUMBING', 'Medium', 'hello', '', '2024-09-13'),
-(13, 46, 'PLUMBING', 'Medium', 'hii', '', '0000-00-00'),
-(14, 47, 'IT INFRA', 'High', 'gyh', '', '0000-00-00'),
-(15, 50, 'PARTITION', 'High', 'hh', '', '0000-00-00'),
-(16, 43, 'CARPENTRY', 'High', 'hello', '', '0000-00-00'),
-(17, 49, 'IT INFRA', 'High', '', '', '0000-00-00');
+INSERT INTO `manager` (`task_id`, `problem_id`, `worker_id`, `priority`, `comment_query`, `comment_reply`, `query_date`, `reply_date`) VALUES
+(48, 63, 'CARPENTRY', 'High', '', '', '0000-00-00', NULL),
+(49, 64, 'CARPENTRY', 'High', '', '', '0000-00-00', NULL),
+(50, 66, 'CARPENTRY', 'Medium', '', '', '0000-00-00', NULL),
+(51, 67, 'CARPENTRY', 'Low', '', '', '0000-00-00', NULL),
+(52, 68, 'CARPENTRY', 'High', '', '', '0000-00-00', NULL),
+(53, 65, 'CIVIL', 'Medium', '', '', '0000-00-00', NULL);
 
 -- --------------------------------------------------------
 
@@ -148,6 +154,7 @@ INSERT INTO `manager` (`task_id`, `problem_id`, `worker_id`, `priority`, `commen
 CREATE TABLE `worker_details` (
   `id` int(11) NOT NULL,
   `worker_id` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `worker_first_name` varchar(100) NOT NULL,
   `worker_last_name` varchar(100) NOT NULL,
   `worker_gender` varchar(100) NOT NULL,
@@ -155,20 +162,21 @@ CREATE TABLE `worker_details` (
   `worker_mobile` varchar(10) NOT NULL,
   `worker_mail` varchar(100) NOT NULL,
   `worker_emp_type` varchar(100) NOT NULL,
-  `worker_photo` varchar(50) NOT NULL
+  `worker_photo` varchar(50) NOT NULL,
+  `usertype` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `worker_details`
 --
 
-INSERT INTO `worker_details` (`id`, `worker_id`, `worker_first_name`, `worker_last_name`, `worker_gender`, `worker_dept`, `worker_mobile`, `worker_mail`, `worker_emp_type`, `worker_photo`) VALUES
-(32, 'ELE01', 'Nivetha(HEAD_1)', 'K', 'Female', 'ELECTRICAL', '9952625820', 'nivethak.vlsi@mkce.ac.in', ' Full-time', ''),
-(33, 'CAR01', 'HEAD_NAME2', '', 'Male', 'CARPENTRY', '9940911511', 'mukeshp.civil@mkce.ac.in', 'Full-time', ''),
-(34, 'CIVIL01', 'HEAD_NAME3', '', 'Male', 'CIVIL', '9159594640', 'karthickr.mech@mkce.ac.in ', 'Full-time', ''),
-(35, 'PAR01', 'HEAD_NAME', '', 'Male', 'PARTITION', '9159594640', 'manirajp.eee@mkce.ac.in', 'Full-time', ''),
-(36, 'PLU01', 'HEAD_NAME04', '', 'Male', 'PLUMBING', '8220638519', 'ranganathanr.maths@mkce.ac.in', 'Full-time', ''),
-(37, 'INFRA01', 'HEAD_NAME05', '', 'Female', 'IT INFRA', '6380153269', 'sathiyanathans.it@mkce.ac.in', 'Part-time', '');
+INSERT INTO `worker_details` (`id`, `worker_id`, `password`, `worker_first_name`, `worker_last_name`, `worker_gender`, `worker_dept`, `worker_mobile`, `worker_mail`, `worker_emp_type`, `worker_photo`, `usertype`) VALUES
+(32, 'ELE01', '123', 'Nivetha', 'K', 'Female', 'ELECTRICAL', '9952625820', 'nivethak.vlsi@mkce.ac.in', ' Full-time', '', 'user'),
+(33, 'CAR01', '123', 'HEAD_NAME2', '', 'Male', 'CARPENTRY', '9940911511', 'mukeshp.civil@mkce.ac.in', 'Full-time', '', 'user'),
+(34, 'CIVIL01', '123', 'HEAD_NAME3', '', 'Male', 'CIVIL', '9159594640', 'karthickr.mech@mkce.ac.in ', 'Full-time', '', 'user'),
+(35, 'PAR01', '123', 'HEAD_NAME', '', 'Male', 'PARTITION', '9159594640', 'manirajp.eee@mkce.ac.in', 'Full-time', '', 'user'),
+(36, 'PLU01', '123', 'HEAD_NAME04', '', 'Male', 'PLUMBING', '8220638519', 'ranganathanr.maths@mkce.ac.in', 'Full-time', '', 'user'),
+(37, 'INFRA01', '123', 'HEAD_NAME05', '', 'Female', 'IT INFRA', '6380153269', 'sathiyanathans.it@mkce.ac.in', 'Part-time', '', 'user');
 
 -- --------------------------------------------------------
 
@@ -189,14 +197,10 @@ CREATE TABLE `worker_taskdet` (
 --
 
 INSERT INTO `worker_taskdet` (`id`, `task_id`, `after_photo`, `task_completion`, `work_completion_date`) VALUES
-(1, 1, '1.jpeg', 'Fully Completed', '2024-08-25'),
-(3, 2, '1.jpeg', 'Partially Completed', '2024-08-25'),
-(4, 3, '3.jpg', 'Fully Completed', '2024-08-25'),
-(5, 43, '2.jpeg', 'Fully Completed', '2024-08-26'),
-(6, 5, '3.jpg', 'Fully Completed', '2024-08-27'),
-(7, 49, '4.jpg', 'Partially Completed', '2024-08-27'),
-(8, 7, '3.jpg', 'Partially Completed', '2024-08-27'),
-(9, 43, '1.jpeg', 'tjyguyh', '2024-09-12');
+(33, 40, 'Screenshot (1).png', 'Fully Completed', '2024-09-24'),
+(34, 41, '1.png', 'Partially Completed', '2024-09-24'),
+(35, 42, '1.png', 'Fully Completed', '2024-09-24'),
+(36, 43, '1.png', 'Fully Completed', '2024-09-24');
 
 --
 -- Indexes for dumped tables
@@ -213,6 +217,12 @@ ALTER TABLE `comments`
 -- Indexes for table `complaints_detail`
 --
 ALTER TABLE `complaints_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -244,19 +254,25 @@ ALTER TABLE `worker_taskdet`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `complaints_detail`
 --
 ALTER TABLE `complaints_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `manager`
 --
 ALTER TABLE `manager`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `worker_details`
@@ -268,23 +284,7 @@ ALTER TABLE `worker_details`
 -- AUTO_INCREMENT for table `worker_taskdet`
 --
 ALTER TABLE `worker_taskdet`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`problem_id`) REFERENCES `complaints_detail` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `manager`
---
-ALTER TABLE `manager`
-  ADD CONSTRAINT `problem_id` FOREIGN KEY (`problem_id`) REFERENCES `complaints_detail` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
